91178d23b41d8ae119f36649ab143b01969c67a7
Felipe-Cjesus <faladorf10@hotmail.com>
2025-06-24 04:09:53 -0300

    deploy tests

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -17,15 +17,15 @@ class UserController extends Controller
      */
     public function index() : JsonResponse
     {
-        // $users = User::orderBy('id', 'DESC')->paginate(15);
-
-        // return response()->json([
-        //     'status' => true,
-        //     'users'  => $users,
-        // ], 200);
+        $users = User::orderBy('id', 'DESC')->paginate(15);
 
         return response()->json([
-            'status' => "OK",
+            'status' => true,
+            'users'  => $users,
         ], 200);
+
+        // return response()->json([
+        //     'status' => "OK",
+        // ], 200);
     }
 }
